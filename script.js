93692b272914ba7bec408852dbc372bbb62b8578
ViraajSingh <write2viraaj@gmail.com>
2024-11-12 18:20:03 -0700

    done
    done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "BOO!!";
   }
+function colors(){
+  document.getElementById("about").innerHTML = "uh oh it is all gone now HEHEHEHE"
+}
